d5eb2c8abfed619cda0becb733421f5cf59775df
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2023-01-08 08:37:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-echo " BUILD START"
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate
